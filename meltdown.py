ee6ff19c8b4e2950d4cec2cbd484341f72425489
Onelightyear <slava.kutuzzi@gmail.com>
2024-10-02 11:13:58 +0300

    meltdown changes

--- meltdown.py
+++ meltdown.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
     shp = get_info(filename)['size'] # shape of the lattice
     Parts_num = get_info(filename)['N'] # number of particles
     Temp = get_info(filename)['temp']
-
+    potential = get_info(filename)['potential']
     particles_init = get_info(filename)['configuration']
-    print('Starting energy', pot_calc(particles_init), '\n')
+    print('Starting energy', pot_calc(particles_init, potential), '\n')
     
     # coords_path = []
     particle_distance = []
@@ -32,18 +32,18 @@ if __name__ == '__main__':
     while i <= iterations:
         i += 1
         part_rand = randomize_1particle(part_now, shp, Parts_num)
-        if jump_estimator(part_now, part_rand, Temp) == True:
+        if jump_estimator(part_now, part_rand, Temp, potential) == True:
             part_inter = smart_randomizer(Parts_num, shp) # intermediate check of random configuration being less in energy (to ensure faster convergence)
-            if jump_estimator(part_rand, part_inter, Temp) == True:
+            if jump_estimator(part_rand, part_inter, Temp, potential) == True:
                 
                 part_now = part_inter
             else:
                 part_now = part_rand
         particle_distance.append(avg_distance(part_now))
-        energy_arr.append(pot_calc(part_now)) 
+        energy_arr.append(pot_calc(part_now, potential)) 
 
             # coords_path.append(part_now)
-    print('Final Energy', pot_calc(part_now), '\n')
+    print('Final Energy', pot_calc(part_now, potential), '\n')
     print('Montecarlo-ed!')
 
     avg_distance_tot = Average_value(particle_distance[1500:], energy_arr[1500:], Temp)
